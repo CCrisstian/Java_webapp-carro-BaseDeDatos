1df4363b54eca10143b71cd1bae1f4cc3e25ac83
CCrisstian <crisstiann.c@gmail.com>
2024-07-25 22:41:13 -0300

    Unidad 48 agregando una conexión a Base de Datos a la aplicación

--- src/main/java/org/CCristian/apiservlet/webapp/headers/filters/ConexionFilter.java
+++ src/main/java/org/CCristian/apiservlet/webapp/headers/filters/ConexionFilter.java
@@ -2,13 +2,33 @@ package org.CCristian.apiservlet.webapp.headers.filters;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.CCristian.apiservlet.webapp.headers.util.ConexionBaseDatos;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebFilter("/*")
 public class ConexionFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
+        try (Connection conn = ConexionBaseDatos.getConection()){
+            if (conn.getAutoCommit()){
+                conn.setAutoCommit(false);
+            }
+            try {
+                request.setAttribute("conn", conn);
+                chain.doFilter(request, response);
+                conn.commit();
+            } catch (SQLException e){
+                conn.rollback();
+                ((HttpServletResponse)response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
